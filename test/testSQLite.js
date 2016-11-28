9e71e736e372c56e0504e0b4554e53ea8f5e1506
Ingo Fischer <github@fischer-ka.de>
2016-11-28 23:08:20 +0100

    enhance tests to check getHistory

--- test/testSQLite.js
+++ test/testSQLite.js
@@ -153,7 +153,25 @@ describe('Test SQLite', function() {
                                             if (result.result[i].val >= 1 && result.result[i].val <= 3) found ++;
                                         }
                                         expect(found).to.be.equal(3);
-                                        done();
+
+                                        sendTo('sql.0', 'getHistory', {
+                                            id: 'system.adapter.history.0.memRss',
+                                            options: {
+                                                end:       now,
+                                                count:     2,
+                                                aggregate: 'onchange'
+                                            }
+                                        }, function (result) {
+                                            console.log(JSON.stringify(result.result, null, 2));
+                                            expect(result.result.length).to.be.at.least(2);
+                                            var found = 0;
+                                            for (var i = 0; i < result.result.length; i++) {
+                                                if (result.result[i].val >= 1 && result.result[i].val <= 3) found ++;
+                                            }
+                                            expect(found).to.be.equal(2);
+                                            done();
+                                        });
+
                                     });
                                 });
                             }, 2000);
