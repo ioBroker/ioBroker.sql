c92cf1eb88ad51d5d7b52e949d3182aca1aab5c7
Ingo Fischer <github@fischer-ka.de>
2016-11-29 07:30:16 +0100

    restructure testing

--- test/testSQLite.js
+++ test/testSQLite.js
@@ -126,7 +126,7 @@ describe('Test SQLite', function() {
         });
     });
     it('Test SQLite: Write values into DB', function (done) {
-        this.timeout(25000);
+        this.timeout(10000);
         var now = new Date().getTime();
 
         states.setState('system.adapter.sql.0.memRss', {val: 1, ts: now - 2000}, function (err) {
@@ -144,42 +144,70 @@ describe('Test SQLite', function() {
                                 console.log(err);
                             }
                             setTimeout(function () {
-                                sendTo('sql.0', 'query', 'SELECT id FROM datapoints WHERE name="system.adapter.sql.0.memRss"', function (result) {
-                                    sendTo('sql.0', 'query', 'SELECT * FROM ts_number WHERE id=' + result.result[0].id, function (result) {
-                                        console.log(JSON.stringify(result.result, null, 2));
-                                        expect(result.result.length).to.be.at.least(3);
-                                        var found = 0;
-                                        for (var i = 0; i < result.result.length; i++) {
-                                            if (result.result[i].val >= 1 && result.result[i].val <= 3) found ++;
-                                        }
-                                        expect(found).to.be.equal(3);
-
-                                        setTimeout(function () {
-                                            sendTo('sql.0', 'getHistory', {
-                                                id: 'system.adapter.sql.0.memRss',
-                                                options: {
-                                                    end:       now,
-                                                    count:     2,
-                                                    aggregate: 'onchange'
-                                                }
-                                            }, function (result) {
-                                                console.log(JSON.stringify(result.result, null, 2));
-                                                expect(result.result.length).to.be.at.least(2);
-                                                var found = 0;
-                                                for (var i = 0; i < result.result.length; i++) {
-                                                    if (result.result[i].val >= 1 && result.result[i].val <= 2) found ++;
-                                                }
-                                                expect(found).to.be.equal(2);
-                                                done();
-                                            });
-                                        }, 3000);
-                                    });
-                                });
+                                done();
                             }, 2000);
                         });
                     }, 500);
                 });
             }, 500);
+        });
+    });
+    it('Test ' + adapterShortName + ': Read values from DB using query', function (done) {
+        this.timeout(10000);
+
+        sendTo('sql.0', 'query', 'SELECT id FROM datapoints WHERE name="system.adapter.sql.0.memRss"', function (result) {
+            sendTo('sql.0', 'query', 'SELECT * FROM ts_number WHERE id=' + result.result[0].id, function (result) {
+                console.log(JSON.stringify(result.result, null, 2));
+                expect(result.result.length).to.be.at.least(3);
+                var found = 0;
+                for (var i = 0; i < result.result.length; i++) {
+                    if (result.result[i].val >= 1 && result.result[i].val <= 3) found ++;
+                }
+                expect(found).to.be.equal(3);
+
+                setTimeout(function () {
+                    done();
+                }, 3000);
+            });
+        });
+    });
+    it('Test ' + adapterShortName + ': Read values from DB using GetHistory', function (done) {
+        this.timeout(10000);
+
+        sendTo('sql.0', 'getHistory', {
+            id: 'system.adapter.sql.0.memRss',
+            options: {
+                start:     now - 1000000,
+                end:       now,
+                count:     50,
+                aggregate: 'onchange'
+            }
+        }, function (result) {
+            console.log(JSON.stringify(result.result, null, 2));
+            expect(result.result.length).to.be.at.least(3);
+            var found = 0;
+            for (var i = 0; i < result.result.length; i++) {
+                if (result.result[i].val >= 1 && result.result[i].val <= 3) found ++;
+            }
+            expect(found).to.be.equal(3);
+
+            sendTo('sql.0', 'getHistory', {
+                id: 'system.adapter.sql.0.memRss',
+                options: {
+                    start:     now - 1000000,
+                    end:       now,
+                    count:     2,
+                    aggregate: 'onchange'
+                }
+            }, function (result) {
+                console.log(JSON.stringify(result.result, null, 2));
+                expect(result.result.length).to.be.at.least(2);
+                var found = 0;
+                for (var i = 0; i < result.result.length; i++) {
+                    if (result.result[i].val >= 1 && result.result[i].val <= 2) found ++;
+                }
+                expect(found).to.be.equal(2);
+                done();
         });
     });
 
