4c17fd33f693f96ffbcf39e97296fe05d1e35d2d
Ingo Fischer <github@fischer-ka.de>
2016-12-04 23:47:14 +0100

    invalid logging caused crash

--- main.js
+++ main.js
@@ -672,12 +672,12 @@ function pushHistory(id, state, timerRelog) {
         if (sqlDPs[id].state && settings.changesOnly && !timerRelog) {
             if (settings.changesRelogInterval === 0) {
                 if (state.ts !== state.lc) {
-                    adapter.log.debug('value not changed ' + id + ', last-value=' + sqlDPs[id].state.val.val + ', new-value=' + state.val + ', ts=' + state.ts);
+                    adapter.log.debug('value not changed ' + id + ', last-value=' + sqlDPs[id].state.val + ', new-value=' + state.val + ', ts=' + state.ts);
                     return;
                 }
             } else if (sqlDPs[id].lastLogTime) {
                 if ((state.ts !== state.lc) && (Math.abs(sqlDPs[id].lastLogTime - state.ts) < settings.changesRelogInterval * 1000)) {
-                    adapter.log.debug('value not changed ' + id + ', last-value=' + sqlDPs[id].state.val.val + ', new-value=' + state.val + ', ts=' + state.ts);
+                    adapter.log.debug('value not changed ' + id + ', last-value=' + sqlDPs[id].state.val + ', new-value=' + state.val + ', ts=' + state.ts);
                     return;
                 }
                 if (state.ts !== state.lc) {
@@ -685,14 +685,14 @@ function pushHistory(id, state, timerRelog) {
                 }
             }
             if ((settings.changesMinDelta !== 0) && (typeof state.val === 'number') && (Math.abs(sqlDPs[id].state.val - state.val) < settings.changesMinDelta)) {
-                adapter.log.debug('Min-Delta not reached ' + id + ', last-value=' + sqlDPs[id].state.val.val + ', new-value=' + state.val + ', ts=' + state.ts);
+                adapter.log.debug('Min-Delta not reached ' + id + ', last-value=' + sqlDPs[id].state.val + ', new-value=' + state.val + ', ts=' + state.ts);
                 return;
             }
             else if (typeof state.val === 'number') {
-                adapter.log.debug('Min-Delta reached ' + id + ', last-value=' + sqlDPs[id].state.val.val + ', new-value=' + state.val + ', ts=' + state.ts);
+                adapter.log.debug('Min-Delta reached ' + id + ', last-value=' + sqlDPs[id].state.val + ', new-value=' + state.val + ', ts=' + state.ts);
             }
             else {
-                adapter.log.debug('Min-Delta ignored because no number ' + id + ', last-value=' + sqlDPs[id].state.val.val + ', new-value=' + state.val + ', ts=' + state.ts);
+                adapter.log.debug('Min-Delta ignored because no number ' + id + ', last-value=' + sqlDPs[id].state.val + ', new-value=' + state.val + ', ts=' + state.ts);
             }
         }
 
