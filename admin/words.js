5937f4b3044d46390e6a8a810e3bd30f9845dc1d
GermanBluefox <dogafox@gmail.com>
2017-07-28 07:53:07 +0200

    (bluefox) cosmetic changes

---
+++ admin/words.js
@@ -0,0 +1,77 @@
+systemDictionary = {
+    "History adapter settings": {
+        "de": "History-Adapter Einstellungen",
+        "ru": "Настройки профайлера"},
+    "note": {
+        "en": "History settings must be done for every state individually on the tab \"States\" and only if adapter is active",
+        "de": "History Einstellungen mussen für jeden State einzeln gemacht werden auf der Lasche \"States\" und nur wenn adapter aktiv ist",
+        "ru": "Настройки профайлера делаются для каждого состояния отдельно в закладке \"Состояния\" и только, когда драйвер активирован"
+    },
+    "Default settings:": {
+        "de": "Default Einstellungen für Zuständen:",
+        "ru": "Настройки по умолчанию для состояний"
+    },
+    "Start or enable adapter first": {
+        "de": "Adapter muss zuerst aktiviert werden",
+        "ru": "Сначала надо активировать драйвер SQL"
+    },
+    "debounce":             {"en": "De-bounce interval(ms)", "de": "Entprellzeit(ms)",              "ru": "Минимальный интервал(ms)"},
+    "log changes interval(s)": {
+        "en": "Log unchanged values any(s)",
+        "de": "Gleiche Werte aufzeichnen(s)",
+        "ru": "Запись неизменённых значений каждые (сек)"
+    },
+    "log changes minimal delta": {
+        "en": "Minimum difference from last value to log",
+        "de": "Minimale Abweichung vom letzten Wert für Aufzeichnung",
+        "ru": "Минимальная разница с последним записанным значением"
+    },
+    "0 = disable delta check": {
+        "en": "0 = disable delta check",
+        "de": "0 = Abweichungsprüfung deaktivieren",
+        "ru": "0 = не проверять минимальное изменение"
+    },
+    "0 = disable": {
+        "en": "0 = disable",
+        "de": "0 = Deaktivieren",
+        "ru": "0 = не активно"
+    },
+    "retention":            {"en": "Storage retention",     "de": "Storage Vorhaltezeit",           "ru": "Время хранения в базе"},
+    "keep forever":         {"en": "keep forever",          "de": "keine automatische Löschung",    "ru": "хранить вечно"},
+    "2 years":              {"en": "2 years",               "de": "2 Jahre",                        "ru": "2 года"},
+    "1 year":               {"en": "1 year",                "de": "1 Jahr",                         "ru": "1 год"},
+    "6 months":             {"en": "6 months",              "de": "6 Monate",                       "ru": "6 месяцев"},
+    "3 months":             {"en": "3 months",              "de": "3 Monate",                       "ru": "3 месяца"},
+    "1 months":             {"en": "1 months",              "de": "1 Monat",                        "ru": "1 месяц"},
+    "2 weeks":              {"en": "2 weeks",               "de": "2 Wochen",                       "ru": "2 недели"},
+    "1 week":               {"en": "1 week",                "de": "1 Woche",                        "ru": "1 неделя"},
+    "5 days":               {"en": "5 days",                "de": "5 Tage",                         "ru": "5 дней"},
+    "3 days":               {"en": "3 days",                "de": "3 Tage",                         "ru": "3 дня"},
+    "1 day":                {"en": "1 day",                 "de": "1 Tag",                          "ru": "1 день"},
+    "SQL history adapter settings": {
+        "en": "SQL history adapter settings",
+        "de": "SQL history adapter Einstellungen",
+        "ru": "Настройки профайлера в SQL базу данных"
+    },
+    "DB settings:":         {"en": "DB settings:",          "de": "DB Einstellungen:",              "ru": "DB settings:"},
+    "DB Type":              {"en": "DB Type",               "de": "DB Typ",                         "ru": "DB Type"},
+    "Host":                 {"en": "Host",                  "de": "Host",                           "ru": "Хост"},
+    "DB Name":              {"en": "Database name",         "de": "Datenbankname",                  "ru": "Имя базы данных"},
+    "Port":                 {"en": "Port",                  "de": "Port",                           "ru": "Порт"},
+    "User":                 {"en": "User",                  "de": "Login",                          "ru": "Пользователь"},
+    "Password":             {"en": "Password",              "de": "Kennwort",                       "ru": "Пароль"},
+    "Password confirm":     {"en": "Password confirm",      "de": "Kennwort-Wiederholung",          "ru": "Подтверждение пароля"},
+    "File for sqlite:":     {"en": "File for sqlite:",      "de": "Datei für sqlite:",              "ru": "Файл для sqlite:"},
+    "Encrypt:":             {"en": "Encrypt:",              "de": "Verschlüsseln:",                 "ru": "Шифрование:"},
+    "Test connection":      {"en": "Test connection",       "de": "Verbindung testen",              "ru": "Проверить соединение"},
+    "requestInterval":      {"en": "Pause between requests(ms)","de": "Intervall zwischen DB Anfragen(ms)",  "ru": "Пауза между запросами к базе"},
+    "Round real to:":       {"en": "Round real to:",        "de": "Aufrunden auf: ",                "ru": "Округлять до"},
+    "Are you sure? All data will be dropped.": {
+        "en": "Are you sure? All data will be dropped.",
+        "de": "Sind Sie sicher? ALLE Daten werden geöscht.",
+        "ru": "Вы уверенны? Все данные в базе будут удалены."
+    },
+    "Reset DB":             {"en": "Reset DB",              "de": "Alle Daten in DB löschen",       "ru": "Сбросить DB"},
+    "Ok":                   {"en": "Ok",                    "de": "Ok",                             "ru": "Ok"},
+    "Allow parallel requests:": {"en": "Allow parallel requests:", "de": "Parallelanfragen erlauben:", "ru": "Разрешить параллельные запросы к базе:"}
+};
